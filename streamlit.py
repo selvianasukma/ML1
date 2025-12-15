1b2b467ab7f1fc1963d0f8158768dc875fd78bfb
selvianasukma09 <sukmaayuselviyana423@gmail.com>
2025-12-15 15:57:09 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -68,7 +68,7 @@ if st.button("Prediksi Produksi"):
     prediction = model.predict(input_array)
 
     st.subheader("Hasil Prediksi")
-    st.success(f"Perkiraan Produksi Padi: **{prediction[0]:,.2f}** KG")
+    st.success(f"Perkiraan Produksi Padi: **{prediction[0]:,.2f}** HEKTAR")
 
     # =============================
     # Grafik Prediksi
@@ -109,6 +109,7 @@ else:
     st.info("Informasi koefisien tidak tersedia untuk model ini")
 
 st.caption("Model dilatih di Jupyter Notebook dan hanya di-load di Streamlit (tanpa training ulang).")
+
 
 
 
